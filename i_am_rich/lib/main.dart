c5ca9f5384945ad8386abca96cd9955438be55af
aoctaviopb <andresperez192@gmail.com>
2023-02-06 12:59:16 -0500

    Proyecto I am rich

---
+++ i_am_rich/lib/main.dart
@@ -0,0 +1,23 @@
+// ignore_for_file: prefer_const_constructors
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: Center(
+            child: Text('I Am Rich'),
+          ),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Center(
+          child: Image(
+            image: AssetImage('images/diamond.png'),
+          ),
+        ),
+      ),
+    ),
+  );
+}
